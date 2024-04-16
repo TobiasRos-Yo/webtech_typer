a3807a19720ee071c4b9388ac6dfb58d20a53173
Tobias Rosenberg <tobias.ros@outlook.de>
2024-04-16 23:02:51 +0200

    added spring project

---
+++ src/test/java/com/example/typer/TyperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.typer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
